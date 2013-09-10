82073f1568aef961f6da44e24efef46635db0f86
Mastergalen <galenhan96@gmail.com>
2013-09-11 01:10:53 +0200

    Update copied credits template

--- StopFakeUsers.php
+++ StopFakeUsers.php
@@ -6,7 +6,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['stopfakeusers'][] = array(
 	'path' => __FILE__,
 	'name' => 'StopFakeUsers',
-	'author' => array( 'Brion Vibber', '...' ),
+	'author' => array( 'Galen Han' ),
 	'url' => 'http://galenhan.com',
 	'version' => '0.1',
 	'descriptionmsg' => 'Stop Fake Users from signing up, by hiding the real name field. Bots will fill out this field. If they do, abort login. '
